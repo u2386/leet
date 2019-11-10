2c3c951896983e4aba966530113c259fbfb0f2d4
hugo <hugo.cavan2386@gmail.com>
2019-11-10 14:41:54 +0800

    复原IP地址

---
+++ external/复原IP地址/solution.py
@@ -0,0 +1,63 @@
+#coding: utf-8
+
+
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        def valid(seg):
+            return int(seg) < 256 if seg[0] != '0' else len(seg) == 1
+
+        def split(pos, count):
+            if count == 0:
+                if valid(s[pos:]):
+                    segments.append(s[pos:])
+                    ret.append('.'.join(segments))
+                    segments.pop()
+                return
+
+            for i in range(pos+1, min(pos+4, len(s))):
+                seg = s[pos:i]
+                if not valid(seg):
+                    return
+
+                segments.append(seg)
+                split(i, count-1)
+                segments.pop()
+
+        segments = []
+        ret = []
+        split(0, 3)
+        return ret
+
+
+def test01():
+    assert Solution().restoreIpAddresses('25525511135') == [
+        '255.255.11.135',
+        '255.255.111.35',
+    ]
+
+
+def test02():
+    assert Solution().restoreIpAddresses('0000') == [
+        '0.0.0.0'
+    ]
+
+
+def test03():
+    assert Solution().restoreIpAddresses('010010') == [
+        '0.10.0.10',
+        '0.100.1.0',
+    ]
+
+
+def main():
+    import pdb
+    pdb.set_trace()
+    print(Solution().restoreIpAddresses('0000'))
+
+
+if __name__ == '__main__':
+    main()
