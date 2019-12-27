58877e07313149d5037d893193edeaa7c2941d40
hugo <hugo.cavan2386@gmail.com>
2019-12-27 14:05:05 +0800

    111-Minimum Depth of Binary Tree

--- 111-Minimum Depth of Binary Tree/solution.go
+++ 111-Minimum Depth of Binary Tree/solution.go
@@ -2,13 +2,42 @@ package main
 
 /**
  * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
  */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 func minDepth(root *TreeNode) int {
+	depth := 0
+
+	if root == nil {
+		return depth
+	}
+
+	q := [][]*TreeNode{}
+	q = append(q, []*TreeNode{root})
+	for {
+		tair := q[0]
+		q = append(q[1:], []*TreeNode{})
+		if len(tair) == 0 {
+			return depth
+		}
+		depth++
+
+		for _, n := range tair {
+			if n == nil || (n.Left == nil && n.Right == nil) {
+				return depth
+			}
+			if n.Left != nil {
+				q[len(q)-1] = append(q[len(q)-1], n.Left)
+			}
+			if n.Right != nil {
+				q[len(q)-1] = append(q[len(q)-1], n.Right)
+			}
+		}
+	}
 
 }
 
