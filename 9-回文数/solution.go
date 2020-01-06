b32f0d68d042bdf82f96c3da3c72b7f49c69403b
hugo <hugo.cavan2386@gmail.com>
2020-01-06 13:39:24 +0800

    9-回文数

---
+++ 9-回文数/solution.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+
+	r := 0
+	for x > r {
+		r = r*10 + x%10
+		x /= 10
+	}
+	return x == r || x == r/10
+}
+
+func main() {
+	fmt.Println(isPalindrome(10))
+}
