be8c054d67cc64f7cdc998f6e5c750423e9ad5e6
hugo <hugo.cavan2386@gmail.com>
2020-01-11 14:06:05 +0800

    179-Largest Number

--- 179-Largest Number/solution.go
+++ 179-Largest Number/solution.go
@@ -3,40 +3,53 @@ package main
 import (
 	"fmt"
 	"sort"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type SortBy []int
 
-func (a SortBy) Len() int           { return len(a) }
-func (a SortBy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a SortBy) Less(i, j int) bool { 
-	x := strconv.Itoa(a[i])
-	y := strconv.Itoa(a[j])
-	if len(x) < len(y) {
-		return false
-	} else if len(x) > len(y) {
-		return true
-	}
-	return x < y
- }
+func (a SortBy) Len() int      { return len(a) }
+func (a SortBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a SortBy) Less(i, j int) bool {
+	var x, y strings.Builder
+
+	x.Reset()
+	y.Reset()
+
+	x.WriteString(strconv.Itoa(a[i]))
+	x.WriteString(strconv.Itoa(a[j]))
+
+	y.WriteString(strconv.Itoa(a[j]))
+	y.WriteString(strconv.Itoa(a[i]))
+
+	return x.String() < y.String()
+}
 
 func largestNumber(nums []int) string {
 	if len(nums) == 0 {
 		return ""
 	} else if len(nums) == 1 {
-		return string(nums[0])
+		return strconv.Itoa(nums[0])
 	}
 
+	flag := true
 	sort.Sort(sort.Reverse(SortBy(nums)))
 	var sb strings.Builder
 	for _, num := range nums {
+		if flag && num == 0 {
+			continue
+		}
+		flag = false
 		sb.WriteString(strconv.Itoa(num))
 	}
-	return sb.String()
+	ret := sb.String()
+	if len(ret) == 0 {
+		return "0"
+	}
+	return ret
 }
 
 func main() {
-	fmt.Println(largestNumber([]int{3,30,34,5,9}))
+	fmt.Println(largestNumber([]int{3, 30, 34, 5, 9}))
 }
