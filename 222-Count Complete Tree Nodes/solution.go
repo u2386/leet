a26a0422e84295f7826d5d8a5babf5c7c925e188
hugo <hugo.cavan2386@gmail.com>
2020-02-24 12:00:33 +0800

    222-Count Complete Tree Nodes

---
+++ 222-Count Complete Tree Nodes/solution.go
@@ -0,0 +1,19 @@
+package main
+
+// TreeNode is TreeNode
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}
+
+func main() {
+
+}
